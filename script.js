9f5325d383820f127971f5485e672beb1d44c9f7
GUSTAVO NASCIMENTO PEREIRA <tecnico26@prosed.local>
2024-12-02 16:43:19 -0300

    Avançando no frontend

--- script.js
+++ script.js
@@ -26,51 +26,19 @@ function mostraInputOutro(selectId, inputDivId) {
   });
 }
 
-//
-// document.querySelector('tbody').addEventListener('change', (event) => {
-//     if (event.target.classList.contains('selectClass')) {
-//       const span = event.target.closest('tr').querySelector('.spanEPI');
-//       const selectedValue = event.target.value;
-//       if (selectedValue) {
-//         span.innerHTML = `<input type="number" inputmode="numeric" class="ca" placeholder="C.A." >`;
-//       } else{
-//         span.innerHTML = '';
-//       }
-//     }
-//   });
-
-
-const selectElements = document.querySelectorAll('.selectClass');
-const inputFields = document.querySelectorAll('.spanEPI');
-
-function mostrarCampoEntrada(elementoSelect, campoEntrada) {
-    const opcaoSelecionada = elementoSelect.options[elementoSelect.selectedIndex].value;
-
-    if (opcaoSelecionada === 'A') {
-        campoEntrada.style.display = 'block';
-    } else {
-        campoEntrada.style.display = 'none';
-    }
-}
-
-selectElements.forEach((selectElement, index) => {
-    selectElement.addEventListener('change', () => {
-        mostrarCampoEntrada(selectElement, inputFields[index]);
-    });
-});
-
-//Habilita o textarea ao clicar no checkbox
-document.querySelector('tbody').addEventListener('change', (event) => {
-    if (event.target.classList.contains('checkRisco')) {
-      const areaDeTexto = event.target.closest('tr').querySelector('.fontes');
-      if (event.target.checked) {
-        areaDeTexto.disabled = false
-      } else {
-        areaDeTexto.disabled = true
+  document.querySelectorAll('tbody').forEach(tbody => {
+    tbody.addEventListener('change', (event) => {
+      if (event.target.classList.contains('checkRisco')) {
+        const areaDeTexto = event.target.closest('tr').querySelector('.fontes');
+        if (event.target.checked) {
+          areaDeTexto.disabled = false;  // Habilita o textarea
+        } else {
+          areaDeTexto.disabled = true;   // Desabilita o textarea
+        }
       }
-    }
+    });
   });
-
+  
 
 mostraInputOutro('caracteristicasAmbiente', 'inputOutro');
 mostraInputOutro('peDireito', 'inputPeDireito');
@@ -78,8 +46,82 @@ mostraInputOutro('paredes', 'inputParedes');
 mostraInputOutro('piso', 'inputPiso');
 mostraInputOutro('cobertura', 'inputCobertura');
 mostraInputOutro('ventilacao', 'inputVentilacao');
-mostrarCampoEntrada('elementoSelect', 'campoEntrada')
 
+//Função que mostra o campo input number ao selecionar o EPI
+function mostraCA() {
+  // Seleciona todos os elementos select com a classe .selectClass
+  const selects = document.querySelectorAll(".selectClass");
+
+  // Itera sobre todos os selects
+  selects.forEach(select => {
+    // Localiza o elemento span associado na mesma linha
+    const epi = select.closest("td").querySelector(".spanEPI");
+
+    if (select.value !== "") {
+      // Adiciona o campo de texto se houver uma seleção válida
+      epi.innerHTML = `<input type="number"  class="form-control no-spin-buttons" placeholder="C.A." inputmode="numeric">`;
+    } else {
+      // Remove o campo de texto se a seleção for vazia
+      epi.innerHTML = "";
+    }
+  });
+}
+
+//Efeito hide tabela
+//Risco físico
+$(document).ready(function() {
+  // Inicialmente, esconda o corpo da tabela
+  $('#tabelaRiscosFisicos').hide();
+
+  // Quando clicar no título da tabela, alternar a visibilidade do corpo
+  $('#tituloRiscoFisico').click(function() {
+    $('#tabelaRiscosFisicos').toggle(); // Alterna entre mostrar e esconder as linhas
+  });
+});
+
+//Risco químico
+$(document).ready(function() {
+  // Inicialmente, esconda o corpo da tabela
+  $('#tabelaRiscosQuimicos').hide();
+
+  // Quando clicar no título da tabela, alternar a visibilidade do corpo
+  $('#tituloRiscoQuimico').click(function() {
+    $('#tabelaRiscosQuimicos').toggle(); // Alterna entre mostrar e esconder as linhas
+  });
+});
+
+//Risco químico
+$(document).ready(function() {
+  // Inicialmente, esconda o corpo da tabela
+  $('#tabelaRiscosBiologicos').hide();
+
+  // Quando clicar no título da tabela, alternar a visibilidade do corpo
+  $('#tituloRiscoBiologico').click(function() {
+    $('#tabelaRiscosBiologicos').toggle(); // Alterna entre mostrar e esconder as linhas
+  });
+});
+
+//Risco ergonômico
+$(document).ready(function() {
+  // Inicialmente, esconda o corpo da tabela
+  $('#tabelaRiscosErgonomicos').hide();
+
+  // Quando clicar no título da tabela, alternar a visibilidade do corpo
+  $('#tituloRiscoErgonomico').click(function() {
+    $('#tabelaRiscosErgonomicos').toggle(); // Alterna entre mostrar e esconder as linhas
+  });
+});
+
+//Risco Acidente
+$(document).ready(function() {
+  // Inicialmente, esconda o corpo da tabela
+  $('#tabelaRiscosAcidentes').hide();
+
+  // Quando clicar no título da tabela, alternar a visibilidade do corpo
+  $('#tituloRiscoAcidente').click(function() {
+    $('#tabelaRiscosAcidentes').toggle(); // Alterna entre mostrar e esconder as linhas
+  });
+});
 
 
 
